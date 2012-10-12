80876bf1ea010619b12abbbb5b62de75ef389f0d
Gregory Liras <gregliras@gmail.com>
2012-10-12 15:32:47 +0300

    added some sanity checks

--- icalPy.py
+++ icalPy.py
@@ -4,26 +4,37 @@
 #* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 # File Name : icalPy.py
 # Creation Date : 15-09-2012
-# Last Modified : Sat 15 Sep 2012 05:37:46 PM EEST
+# Last Modified : Fri 12 Oct 2012 03:32:30 PM EEST
 # Created By : Greg Liras <gregliras@gmail.com>
 #_._._._._._._._._._._._._._._._._._._._._.*/
 
 from icalendar import Calendar, Event
 from sys import stdin, argv
 
+def safe_get_na(c, evaluated):
+    try:
+        a = eval(evaluated)
+    except KeyError:
+        a = "NA"
+    return a
+
 def main():
     cal = Calendar.from_ical(stdin.read())
     for c in cal.walk():
         if c.name == 'VEVENT':
-            print "Organizer:\t\t", c['ORGANIZER'][7:]
-            print "Summary:\t\t", c['SUMMARY']
+            print "Organizer:\t\t", safe_get_na(c, "c['ORGANIZER'][7:]")
+            print "Summary:\t\t", safe_get_na(c, "c['SUMMARY']")
             print "Attendee(s):"
-            for a in c['ATTENDEE']:
-                print "\t\t\t", a[7:]
-            print "From:\t\t\t", c['DTSTART'].dt
-            print "To:\t\t\t", c['DTEND'].dt
-            print "Where:\t\t\t", c['LOCATION']
-            print "Description:\t\t", c['DESCRIPTION'].encode('latin-1', 'ignore')
+            attend = safe_get_na(c, "c['ATTENDEE']")
+            if type(attend) == list:
+                for a in attend:
+                    print "\t\t\t", a[7:]
+            else:
+                print "\t\t\t", a
+            print "From:\t\t\t", safe_get_na(c, "c['DTSTART'].dt")
+            print "To:\t\t\t", safe_get_na(c, "c['DTEND'].dt")
+            print "Where:\t\t\t", safe_get_na(c, "c['LOCATION']")
+            print "Description:\t\t", safe_get_na(c, "c['DESCRIPTION'].encode('latin-1', 'ignore')")
 
 if __name__=="__main__":
     main()
